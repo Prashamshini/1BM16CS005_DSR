9746057b1fbe0b2f3aa6c070d40a834fdb9155de
Prashamshini <47409882+Prashamshini@users.noreply.github.com>
2019-09-12 06:07:40 +0530

    Update Prog3b.R

--- Prog3b.R
+++ Prog3b.R
@@ -1,9 +1,11 @@
-num <- 19
-str.ds <- "Data Science with R"
-realnum.vec <- c(100.9, 99.01, 90.91)
+num <- 19 # type numeric
+str.ds <- "Data Science with R" # type string
+realnum.vec <- c(100.9, 99.01, 90.91) # type real numbers
 
+# create a list of data objets of type numeric, string and real numbers
 list3b <- list(num, str.ds, realnum.vec)
 list3b
 
+# name the data objects in the list
 names(list3b) <- c("Numeric", "String", "Real Numbers")
 list3b
