a15f81ce1aa8418f75fc76e774e5d087bc68c10b
Prashamshini <47409882+Prashamshini@users.noreply.github.com>
2019-09-12 05:45:30 +0530

    Update Prog2.R

--- Prog2.R
+++ Prog2.R
@@ -5,8 +5,10 @@ red.tail <- c(18,9,12,4,-1)
 carder.bee <- c(8,27,6,32,23)
 honey.bee <- c(12,13,16,9,10)
 
+# join the vectors to make dataframe
 bees.frame <- data.frame(buff.tail, garden.bee, red.tail, carder.bee, honey.bee)
 bees.frame
 
+# create row labels for the data frame
 rownames(bees.frame) <- c("Thistle", "Vipers", "Golden Rain", "Yellowalfala", "Blackberry")
 bees.frame
