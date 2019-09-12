092d38908ced4d129c2ba78fff4dc173f8b8e7b0
Prashamshini <47409882+Prashamshini@users.noreply.github.com>
2019-09-12 06:05:24 +0530

    Create Prog3b.R

---
+++ Prog3b.R
@@ -0,0 +1,9 @@
+num <- 19
+str.ds <- "Data Science with R"
+realnum.vec <- c(100.9, 99.01, 90.91)
+
+list3b <- list(num, str.ds, realnum.vec)
+list3b
+
+names(list3b) <- c("Numeric", "String", "Real Numbers")
+list3b
