01b5e87bb43e35856ea4ec215b811d3264c1aa27
Prashamshini <47409882+Prashamshini@users.noreply.github.com>
2019-09-12 05:44:58 +0530

    Update and rename 2.R to Prog2.R

--- Prog2.R
+++ Prog2.R
@@ -1,9 +1,12 @@
+# make five simple numeric vectors
 buff.tail <- c(10,1,37,5,12)
 garden.bee <- c(8,3,9,6,4)
 red.tail <- c(18,9,12,4,-1)
 carder.bee <- c(8,27,6,32,23)
 honey.bee <- c(12,13,16,9,10)
+
 bees.frame <- data.frame(buff.tail, garden.bee, red.tail, carder.bee, honey.bee)
 bees.frame
+
 rownames(bees.frame) <- c("Thistle", "Vipers", "Golden Rain", "Yellowalfala", "Blackberry")
 bees.frame
